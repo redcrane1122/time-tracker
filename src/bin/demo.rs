aed2ef2f2683ed2aa23af5b7b6a281d598ae8c5b
redcrane1122 <redcrane1122@gmail.com>
2025-11-14 16:58:56 +0900

    added more examples

--- src/bin/demo.rs
+++ src/bin/demo.rs
@@ -3,7 +3,13 @@ fn math(a: i32, b: i32, op: Box<dyn Fn(i32, i32) -> i32>) -> i32 {
 }
 
 fn main() {
-    let add = |a, b| a + b;
-    let add: Box<_> = Box::new(add);
+    let add = Box::new(|a, b| {
+        println!("adding!");
+        a + b
+    });
+    let sub = Box::new(|a, b| a - b);
+    let mul = Box::new(|a, b| a * b);
     println!("{}", math(2, 2, add));
+    println!("{}", math(2, 2, sub));
+    println!("{}", math(2, 2, mul));
 }
